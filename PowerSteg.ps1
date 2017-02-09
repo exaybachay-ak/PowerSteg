95caa060e77f48eda62b41304ca8acdfcb766121
Jacob Kelley <exaybachay.ak@gmail.com>
2017-02-08 22:24:13 -0900

    Update PowerSteg.ps1
    Fixing comments to be in line with indentation levels

--- PowerSteg.ps1
+++ PowerSteg.ps1
@@ -62,10 +62,10 @@ param (
 #############################################################################################################
 #>
 
-################################################----> Modifying script for Windows default PATH functionality
+####----> Modifying script for Windows default PATH functionality
 $testpath = test-path "/bin/"
 
-#####################################################################----> If there is no /bin/, it's Windows
+####----> If there is no /bin/, it's Windows
 if($testpath -ne "True"){
 	$tmppath = (pwd).path
 	$InFile = $tmppath + '\' + $InFile
@@ -73,7 +73,7 @@ if($testpath -ne "True"){
 	$OutFile = $tmppath + '\' + $OutFile
 }
 
-#######################################################----> Otherwise it is Linux so you need / instead of \
+####----> Otherwise it is Linux so you need / instead of \
 if($testpath -eq "True"){
 	$tmppath = (pwd).path
 	$InFile = $tmppath + '/' + $InFile
@@ -81,7 +81,7 @@ if($testpath -eq "True"){
 	$OutFile = $tmppath + '/' + $OutFile	
 }
 
-############################################################################----> read bytes from input file
+####----> read bytes from input file
 $bytes = [System.IO.File]::ReadAllBytes($InFile)
 
 <#
@@ -89,7 +89,7 @@ $bytes = [System.IO.File]::ReadAllBytes($InFile)
 ///////////////////----Enstegcmd function----///////////////////
 ////////////////////////////////////////////////////////////////
 #>
-##############################################################----> if no desteg flag is set, ensteg the file
+####----> if no desteg flag is set, ensteg the file
 if(!$destegcmd -AND !$destegraw){
 	<#
 	///////////////////----Set up steg header----///////////////////
@@ -99,13 +99,13 @@ if(!$destegcmd -AND !$destegraw){
 	///////////////////----Add steg data length field----///////////////////
 	#>
 
-###################################----> read in steg data --- need this first to add steg data length field
+	####----> read in steg data --- need this first to add steg data length field
 	$stegbytes = [System.IO.File]::ReadAllBytes($StegFile)
 
-#############################################################----> make array to store new stegged file data
+	####----> make array to store new stegged file data
 	$faststeg = @()
 
-###########################################################################----> add steg header length info
+	####----> add steg header length info
 	$stegdatalen = $stegbytes.length
 	$stegdatalen = [convert]::tostring($stegdatalen, 2).padleft(32, "0")
 	$faststeg += ,$stegdatalen
@@ -114,14 +114,14 @@ if(!$destegcmd -AND !$destegraw){
 	///////////////////----Add steg extension field----///////////////////
 	#>
 
-###############################################################################----> get file extension info
+	####----> get file extension info
 	$ext = $Stegfile -replace "\.\/",""
 	$reg = [regex]"(\......$|\.....$|\....$)"
 	$extension = $reg.match($ext)
 	$extension = $extension.Captures[0].value
 	$extension = $extension -replace "\.", ""
 
-##################################################################----> add extension to 4-byte header array
+	####----> add extension to 4-byte header array
 	$extarr = @()
 	$e = 0
 	while ($e -le $extension.length-1){
@@ -144,22 +144,22 @@ if(!$destegcmd -AND !$destegraw){
 	///////////////////----Convert steg data from given stegfile----///////////////////
 	#>
 
-#####################################################----> read in the command to steg into our output file
+	####----> read in the command to steg into our output file
 	foreach ($b in $stegbytes){
 		$b = [convert]::tostring($b, 2).padleft(9, "0")
 		$faststeg += $b
 	}
 
-#################################################----> remove spaces and prep array for inserting into file
+	####----> remove spaces and prep array for inserting into file
 	$faststeg = [system.string]::Join("",($faststeg))
 
 	<#
 	///////////////////----Provision array and start populating steg outfile----///////////////////
 	#>
 
-##########################################----> make an array for the new file, and put header info into it
+	####----> make an array for the new file, and put header info into it
 	$newfile = New-Object System.Collections.Generic.List[System.Decimal]
-######################################################################----> populate array for bit shifting
+	####----> populate array for bit shifting
 	for ($a=0; $a -lt 54; $a++){
 		$newfile += ,$bytes[$a]
 	}
@@ -183,29 +183,29 @@ if(!$destegcmd -AND !$destegraw){
 	///////////////////----Begin actual steg operations, inserting bits into data----///////////////////
 	#>
 
-####################################################----> set up countdown variables for embedding steg data
+	####----> set up countdown variables for embedding steg data
 	$stegcountdown = $newarray.length + 54
 	$n = 0
 	$f = 54
-#############################################################----> iterator loop for reconstructing new file
+	####----> iterator loop for reconstructing new file
 	while($f -lt $bytes.length){
-############################################################################----> insert steg data into file
+		####----> insert steg data into file
 		while($f -le $stegcountdown){
-#########################################################----> check if the number is even, meaning LSB is 0 
+			####----> check if the number is even, meaning LSB is 0 
 			if($bytes[$f] % 2 -eq 0){
-###########################################----> now check if steg data is 1.  if it is, adjust newfile by 1
+				####----> now check if steg data is 1.  if it is, adjust newfile by 1
 				if($newarray[$n] -eq 1){
 					$curbyte = $bytes[$f] + 1
 					$newfile += ,$curbyte
 					$f += 1
 				}
-#######################################################----> if steg data is 0 just insert actual image data
+				####----> if steg data is 0 just insert actual image data
 				else{
 					$newfile += ,$bytes[$f]
 					$f += 1
 				}
 			}
-#########################################################----> if the number is not even... meaning LSB is 1
+			####----> if the number is not even... meaning LSB is 1
 			else{
 				if($newarray[$n] -eq 1){
 					$newfile += ,$bytes[$f]
@@ -219,7 +219,7 @@ if(!$destegcmd -AND !$destegraw){
 			}
 			$n += 1
 		}
-############################################----> finish up writing the file without modifying bytes anymore
+		####----> finish up writing the file without modifying bytes anymore
 		$newfile += ,$bytes[$f]
 		$f += 1
 	}
@@ -232,35 +232,35 @@ if(!$destegcmd -AND !$destegraw){
 ///////////////////----Destegcmd function----///////////////////
 ////////////////////////////////////////////////////////////////
 #>
-#############################################----> if parameter is set, desteg command from the stegged file
+####----> if parameter is set, desteg command from the stegged file
 elseif($destegcmd){
-#################################################################----> Start reading after end of BMP header
+	####----> Start reading after end of BMP header
 	$f = 54
-####################################################----> Set end of loop to be after steg size (54 plus 31)
+	####----> Set end of loop to be after steg size (54 plus 31)
 	$sizeloop = 85
-###############################################----> Set end of loop to be after steg extension (85 plus 28)
+	####----> Set end of loop to be after steg extension (85 plus 28)
 	$extloop = 113
 
 	<#
 	///////////////////----Retrieve steg data size----///////////////////
 	#>
-#########################################################----> Make an array for compiling info about header
+	####----> Make an array for compiling info about header
 	$steglength = @()
 	$stegmultiple = @()
 
-####################################################----> Loop through steg header length and put into array
+	####----> Loop through steg header length and put into array
 	while($f -le $sizeloop){
-#############################################################################----> Check to see if byte is 1
+		####----> Check to see if byte is 1
 		if($bytes[$f] -eq 1){
 			$steglength += 1
 			$f += 1
 		}
-##############################################################################----> Check to see if LSB is 0
+		####----> Check to see if LSB is 0
 		elseif($bytes[$f] % 2 -eq 0){
 			$steglength += 0
 			$f += 1
 		}
-####################################################################################----> Otherwise LSB is 1
+		####----> Otherwise LSB is 1
 		else{
 			$steglength += 1
 			$f += 1
@@ -271,23 +271,23 @@ elseif($destegcmd){
 	<#
 	///////////////////----Retrieve steg extension data----///////////////////
 	#>
-########################################################----> Make arrays for compiling info about extension
+	####----> Make arrays for compiling info about extension
 	$extension = @()
 	$extfinal = @()
 
-###################################################----> Loop through steg extension data and put into array
+	####----> Loop through steg extension data and put into array
 	while($f -le $extloop){
-#############################################################################----> Check to see if byte is 1
+		####----> Check to see if byte is 1
 		if($bytes[$f] -eq 1){
 			$extension += 1
 			$f += 1
 		}
-##############################################################################----> Check to see if LSB is 0
+		####----> Check to see if LSB is 0
 		elseif($bytes[$f] % 2 -eq 0){
 			$extension += 0
 			$f += 1
 		}
-####################################################################################----> Otherwise LSB is 1
+		####----> Otherwise LSB is 1
 		else{
 			$extension += 1
 			$f += 1
@@ -295,7 +295,7 @@ elseif($destegcmd){
 	}
 	$extension = [system.string]::Join("",($extension))
 
-############################################################----> Make arrays to store extension information
+	####----> Make arrays to store extension information
 	$extbyte1 = @()
 	$extbyte2 = @()
 	$extbyte3 = @()
@@ -321,7 +321,7 @@ elseif($destegcmd){
 		}
 	}
 
-##############################################################----> Flatten bytes and convert to ascii chars
+	####----> Flatten bytes and convert to ascii chars
 	$extbyte1 = [system.string]::Join("",($extbyte1))
 	$extbyte2 = [system.string]::Join("",($extbyte2))
 	$extbyte3 = [system.string]::Join("",($extbyte3))
@@ -332,7 +332,7 @@ elseif($destegcmd){
 	$extbyte3 = [convert]::tobyte($extbyte3,2)
 	$extbyte4 = [convert]::tobyte($extbyte4,2)
 
-########################################################################----> Check the contents of extbyte1
+	####----> Check the contents of extbyte1
 	if($extbyte1 -eq '0'){
 		$extbyte2 = [convert]::tochar($extbyte2)
 		$extbyte3 = [convert]::tochar($extbyte3)
@@ -361,10 +361,10 @@ elseif($destegcmd){
 	///////////////////----Retrieve steg data----///////////////////
 	#>
 
-######################################################################----> Create array to put steg data in
+	####----> Create array to put steg data in
 	$stegdata = @()
 
-####################################################----> Loop through and get data for insertion into array
+	####----> Loop through and get data for insertion into array
 	$steglength = $steglength * 9
 	$stegend = $f + $steglength
 	$stegcounter = 0
@@ -431,7 +431,7 @@ elseif($destegcmd){
 ///////////////////----Destegraw function----///////////////////
 ////////////////////////////////////////////////////////////////
 #>
-###############################################----> if parameter is set, desteg the data from stegged file
+####----> if parameter is set, desteg the data from stegged file
 elseif($destegraw){
 	write-host "Add in destegraw functionality for data exfil"
 }
